f6257eb492de34f867a1987915d9aa94175c087b
iwata <hirokidrausa0818@gmail.com>
2018-02-27 15:13:07 +0900

    getmovpostのpythonファイル

---
+++ getmovpost.py
@@ -0,0 +1,23 @@
+import os
+import json
+import base64
+
+from bottle import get, response
+from bottle import view, template
+
+@get('/getmovpost')
+@view('getmovpost')
+def entry():
+	return dict(message="hello,bottle")
+
+@get('/api/getmovpost')
+def entry():
+
+	inputfile = os.getcwd()+'/movpost.jpeg'
+	outposts = open(inputfile,'r')
+	outpost = outposts.read()
+	encbody = base64.b64encode(outpost)
+	response.set_header("Content-Type", "application/json;charset=UTF-8")
+	respmsg = { "currentmov" : outpost }
+
+	return json.dumps(respmsg, ensure_ascii=False)
