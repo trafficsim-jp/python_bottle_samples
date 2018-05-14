713f92514c9a874ff1e9196be55b5a702ef86c1a
tsim-kaz <kaz@trafficsim.co.jp>
2018-05-14 20:18:46 +0900

    nginx+uwsgi+bottleのサンプル

---
+++ nginx_uwsgi/hello.py
@@ -0,0 +1,10 @@
+from bottle import route, run, template, default_app
+
+@route('/hello/<name>')
+def index(name):
+	return template('<b>Hello {{name}}</b>!', name=name)
+
+if __name__ == '__main__':
+	run(host='0.0.0.0', port=8080)
+else:
+	application = default_app()
