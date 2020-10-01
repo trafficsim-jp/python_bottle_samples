ab808a60feca892cadb1b7543322f81365cc6779
TANIGUCHI, Kazushige <kaz@trafficsim.co.jp>
2020-10-01 15:44:21 +0900

    ajaxにてRESTFUL APIを呼び出されるように.

--- webmail/assets/js/setting.js
+++ webmail/assets/js/setting.js
@@ -1,4 +1,124 @@
+
+function submit_server_setting(){
+
+}
+
+function init_page(){
+
+	$.ajax({
+		type: 'GET',
+		url:RESTURIROOT+'/sendmail/smtp_server',
+		dataType: 'json',
+		success:(res) => {
+		},
+		error: (req,status,err) => {
+			if(req.status == 404){
+				//設定されていない
+			}
+		},
+		complete: () => {
+		}
+	});
+
+	$.ajax({
+		type: 'GET',
+		url:RESTURIROOT+'/sendmail/smtp_server_port',
+		dataType: 'json',
+		success:(res) => {
+		},
+		error: (req,status,err)=> {
+			if(req.status == 404){
+				// 設定されていない
+				// default値を入れておく
+				let smtp_server_port_input = $("input#smtp_port");
+				smtp_server_port_input.val(587);
+			}
+		},
+		complete: () => {
+		}
+	});
+
+	$.ajax({
+		type: 'GET',
+		url: RESTURIROOT+'/sendmail/username',
+		dataType: 'json',
+		success:(res)=> {
+			let username_input = $("input#inputusername");
+			if (res.username == "") {
+				username_input.val(res.username);
+			}
+			console.log(res);
+		},
+		error: (req,status,err) => {
+			if(req.status == 404){
+				//設定されていない
+			}
+			else {
+			}
+		},
+		complete: ()  => {
+		}
+	});
+
+	$.ajax({
+		type: 'GET',
+		url: RESTURIROOT+'/sendmail/password',
+		dataType: 'json',
+		success:(res) => {
+		},
+		error: (req,status,err) => {
+			if(req.status == 404){
+				//設定されていない
+			}
+			else {
+			}
+		},
+		complete: ()=>{
+		}
+	});
+
+	$.ajax({
+		type: 'GET',
+		url: RESTURIROOT+'/sendmail/from_address',
+		dataType: 'json',
+		success:(res) => {
+		},
+		error: (req,status,err) => {
+			if(req.status == 404){
+				//設定されていない
+			}
+			else {
+			}
+		},
+		complete: ()=>{
+		}
+	});
+
+	$.ajax({
+		type: 'GET',
+		url: RESTURIROOT+'/sendmail/dest_addresses',
+		dataType: 'json',
+		success:(res) => {
+		},
+		error: (req,status,err) => {
+			if(req.status == 404){
+				//設定されていない
+			}
+			else {
+			}
+		},
+		complete: ()=>{
+		}
+	});
+}
+
 jQuery(()=>{
 	console.log("load setting.js");
 	console.log(RESTURIROOT);
+
+	init_page();
+
+	$("button#server_setting").click(()=>{
+		submit_server_setting();
+	});
 });
--- webmail/sendmail_api.py
+++ webmail/sendmail_api.py
@@ -1,14 +1,163 @@
-import json
+# -*- coding: utf-8 -*-
+import os,json
 
 from bottle import Bottle
-from bottle import get, post
+from bottle import get, post, response
 
 import http_settings
+
+CONFIG_JSON_PATH=os.getcwd()+'/conf/sendmail_conf.json'
 
 app = Bottle()
 app.add_hook('after_request', http_settings.enable_cors)
 
+@app.get('/smtp_server')
+def entry():
+	smtp_server=""
+	try:
+		print(CONFIG_JSON_PATH)
+		with open(CONFIG_JSON_PATH, mode='r') as f:
+			conf_json = json_load(f)
+			print(conf_json)
+	except IOError as e:
+		if (e.errno == 2):
+			# まだ設定されていない.
+			response.status = 404
+			response.content_type = 'application/json'
+			return json.dumps({'error' : 'not found'})
+		else:
+			# 何らかのErrorで読めない
+			response.status = 500
+			response.content_type = 'application/json'
+			return json.dumps({'error' : e.message})
+	finally:
+		pass
+
+	return json.dumps({"smtp_server": smtp_server})
+
+@app.get('/smtp_server_port')
+def entry():
+	port=0
+	try:
+		print(CONFIG_JSON_PATH)
+		with open(CONFIG_JSON_PATH, mode='r') as f:
+			conf_json = json_load(f)
+			print(conf_json)
+	except IOError as e:
+		if (e.errno == 2):
+			# まだ設定されていない.
+			response.status = 404
+			response.content_type = 'application/json'
+			return json.dumps({'error' : 'not found'})
+		else:
+			# 何らかのErrorで読めない
+			response.status = 500
+			response.content_type = 'application/json'
+			return json.dumps({'error' : e.message})
+	finally:
+		pass
+
+	return json.dumps({"smtp_server_port": port})
+
 @app.get('/username')
 def entry():
-	return json.dumps({"username" : "" })
+	username=""
 
+	try:
+		print(CONFIG_JSON_PATH)
+		with open(CONFIG_JSON_PATH, mode='r') as f:
+			conf_json = json_load(f)
+			print(conf_json)
+
+	except IOError as e:
+		if (e.errno == 2):
+			# まだ設定されていない.
+			response.status = 404
+			response.content_type = 'application/json'
+			return json.dumps({'error' : 'not found'})
+		else:
+			# 何らかのErrorで読めない
+			response.status = 500
+			response.content_type = 'application/json'
+			return json.dumps({'error' : e.message})
+	finally:
+		pass
+
+	return json.dumps({"username" : username})
+
+
+@app.get('/password')
+def entry():
+
+	password=""
+	try:
+		print(CONFIG_JSON_PATH)
+		with open(CONFIG_JSON_PATH, mode='r') as f:
+			conf_json = json_load(f)
+			print(conf_json)
+
+	except IOError as e:
+		if (e.errno == 2):
+			# まだ設定されていない.
+			response.status = 404
+			response.content_type = 'application/json'
+			return json.dumps({'error' : 'not found'})
+		else:
+			# 何らかのErrorで読めない
+			response.status = 500
+			response.content_type = 'application/json'
+			return json.dumps({'error' : e.message})
+	finally:
+		pass
+
+	return json.dumps({"password": password})
+
+@app.get('/from_address')
+def entry():
+	from_address=""
+	try:
+		print(CONFIG_JSON_PATH)
+		with open(CONFIG_JSON_PATH, mode='r') as f:
+			conf_json = json_load(f)
+			print(conf_json)
+
+	except IOError as e:
+		if (e.errno == 2):
+			# まだ設定されていない.
+			response.status = 404
+			response.content_type = 'application/json'
+			return json.dumps({'error' : 'not found'})
+		else:
+			# 何らかのErrorで読めない
+			response.status = 500
+			response.content_type = 'application/json'
+			return json.dumps({'error' : e.message})
+	finally:
+		pass
+
+	return json.dumps({"from_address": from_address})
+
+@app.get('/dest_addresses')
+def entry():
+	dest_address=""
+	try:
+		print(CONFIG_JSON_PATH)
+		with open(CONFIG_JSON_PATH, mode='r') as f:
+			conf_json = json_load(f)
+			print(conf_json)
+
+	except IOError as e:
+		if (e.errno == 2):
+			# まだ設定されていない.
+			response.status = 404
+			response.content_type = 'application/json'
+			return json.dumps({'error' : 'not found'})
+		else:
+			# 何らかのErrorで読めない
+			response.status = 500
+			response.content_type = 'application/json'
+			return json.dumps({'error' : e.message})
+	finally:
+		pass
+
+	return json.dumps({"dest_addresses": dest_addresses})
