6fb0e04036255e9282ea8b40a6f736737ab85b04
TANIGUCHI, Kazushige <kaz@trafficsim.co.jp>
2020-10-05 16:20:24 +0900

    LINE notifyの利用のためのaccess tokenの登録とそのaccess tokenを利用して投稿するように.

--- line_notify/assets/js/register.js
+++ line_notify/assets/js/register.js
@@ -1,4 +1,49 @@
 
+function init_page() {
+	$.ajax({
+		type: 'GET',
+		url: RESTURIROOT+'/line_notify/access_token',
+		dataType: 'json',
+		success:(res) => {
+			$("input#token").val(res.access_token);
+		},
+		error:(req, status, err) => {
+			if(req.status == 404){
+				//設定されていない
+			}
+			else {
+			}
+		},
+		complete: ()=>{
+		}
+	});
+}
+
+function submit_register(){
+	let access_token = $("input#token").val();
+	$.ajax({
+		type: 'POST',
+		contentType: 'application/json',
+		dataType: 'json',
+		url: RESTURIROOT+'/line_notify/access_token',
+		data: JSON.stringify({access_token : access_token}),
+		success:(res) => {
+			alert("success to register access token");
+		},
+		error:(req, status, err) => {
+			alert("failed to register access token");
+		},
+		complete:() => {
+			init_page();
+		}
+	});
+}
+
+
 jQuery(()=>{
 	console.log("load register page");
+	init_page();
+	$("button#register").click(() => {
+		submit_register();
+	});
 });
--- line_notify/line_notify_api.py
+++ line_notify/line_notify_api.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import os,datetime,json
+import sys,os,datetime,json
 
 from bottle import Bottle
 from bottle import get, post, request, response
@@ -39,26 +39,76 @@ app.add_hook('after_request', http_settings.enable_cors)
 @app.post('/notify')
 def entry():
 	# LINE グループへの投稿
-	print "POST notify"
 	try:
 		conf_obj = load_config_file();
-		req_headers = {
-			'Content-Type'  : 'application/x-www-form-urlencoded',
-			'Authorization' : 'Bearer ' + ACCESS_TOKEN
-		}
 		payload = {'message' : 'current notify test'}
 		payload = urllib.urlencode(payload).encode("utf-8")
 		req = urllib2.Request(LINE_NOTIFY_APIURI)
 		req.add_header('Content-Type', 'application/x-www-form-urlencoded')
-		req.add_header('Authorization', 'Bearer ' + ACCESS_TOKEN)
+		req.add_header('Authorization', 'Bearer ' + conf_obj['access_token'].strip())
 		req.add_data(payload)
 
 		resp_handler = urllib2.urlopen(req)
 		resp = resp_handler.read()
 		print (resp)
 
-	except Exception as e:
-		print "Exception Error:," , e
+	except KeyError as e:
+		response.status = 500
+		response.content_type = 'application/json'
+		return json.dumps({'error' : e.message + " is missing"})
+
+	except:
+		#print "Unexpect except:",sys.exc_info()[0]
+		#print "Unexpect except:",sys.exc_info()[1]
+		response.content_type = 'application/json'
+		return json.dumps({'error' : sys.exc_info()[1]})
 
 	return json.dumps({"result" : "success"})
 
+@app.get('/access_token')
+def entry():
+	access_token=""
+	try:
+		conf_obj = load_config_file()
+		access_token = conf_obj['access_token']
+
+	except IOError as e:
+		if (e.errno == 2):
+			# まだ設定されていない.
+			response.status = 404
+			response.content_type = 'application/json'
+			return json.dumps({'error' : 'not found'})
+		else:
+			# 何らかのErrorで読めない
+			response.status = 500
+			response.content_type = 'application/json'
+			return json.dumps({'error' : e.message})
+	except KeyError:
+		response.status = 500
+		response.content_type = 'application/json'
+		return json.dumps({'error' : e.message})
+	finally:
+		pass
+
+	return json.dumps({"access_token": access_token})
+
+@app.post('/access_token')
+def entry():
+
+	try:
+		conf_obj = load_config_file()
+		conf_obj['access_token'] = request.json['access_token']
+		with open(CONFIG_JSON_PATH, mode='w') as f:
+			json.dump(conf_obj, f)
+
+	except IOError as e:
+		response.status = 500
+		response.content_type = 'application/json'
+		return json.dumps({'error' : e.message})
+
+	except KeyError:
+		response.status = 400
+		response.content_type = 'application/json'
+		return json.dumps({'error' : 'invalid format'})
+
+	return json.dumps({"result": "success"})
